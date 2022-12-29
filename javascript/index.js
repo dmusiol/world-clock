16ed5f58df888a5f7eacce1aa1e25f10320ad42f
Domi <dominika@nozbe.com>
2022-12-29 18:04:01 +0100

    Add the functionality for Select

--- javascript/index.js
+++ javascript/index.js
@@ -2,22 +2,46 @@ setInterval(() => {
     // London
 
 let londonElement = document.querySelector("#london");
+if (londonElement) {
 let londonDateElement = londonElement.querySelector(".date");
 let londonTimeElement = londonElement.querySelector(".time");
 let londonTime = moment().tz("Europe/London");
 
-londonDateElement.innerHTML = londonTime.format("dddd Do YYYY");
-londonTimeElement.innerHTML = `${londonTime.format("H:mm:ss [<small>]A[</small>]")}`;
-
+londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+londonTimeElement.innerHTML = `${londonTime.format("h:mm:ss [<small>]A[</small>]")}`;
+}
 
 // New York
 
 let newYorkElement = document.querySelector("#new-york");
+if (newYorkElement) {
 let newYorkDateElement = newYorkElement.querySelector(".date");
 let newYorkTimeElement = newYorkElement.querySelector(".time");
 let newYorkTime = moment().tz("America/New_York");
 
-newYorkDateElement.innerHTML = newYorkTime.format("dddd Do YYYY");
-newYorkTimeElement.innerHTML = `${newYorkTime.format("H:mm:ss [<small>]A[</small>]")}`;
+newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
+newYorkTimeElement.innerHTML = `${newYorkTime.format("h:mm:ss [<small>]A[</small>]")}`;
+}
 
 }, 1000);
+
+function updateCity(event) {
+    let cityTimeZone = event.target.value;
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `
+    <div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
+        </div>`;
+
+}
+
+
+let citySelect = document.querySelector("#city");
+
+citySelect.addEventListener("change", updateCity);

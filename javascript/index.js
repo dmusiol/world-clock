1d8e9b7e60ee5d63b35e0651bc60318f410a587c
Domi <dominika@nozbe.com>
2022-12-29 18:46:23 +0100

    Update time when city selected

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,4 @@
-setInterval(() => {
+function displayTime() {
     // London
 
 let londonElement = document.querySelector("#london");
@@ -23,16 +23,41 @@ newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
 newYorkTimeElement.innerHTML = `${newYorkTime.format("h:mm:ss [<small>]A[</small>]")}`;
 }
 
-}, 1000);
+// Tokyo
+let tokyoElement = document.querySelector("#tokyo");
+if (tokyoElement) {
+let tokyoDateElement = tokyoElement.querySelector(".date");
+let tokyoTimeElement = tokyoElement.querySelector(".time");
+let tokyoTime = moment().tz("Asia/Tokyo");
 
-function updateCity(event) {
+tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+tokyoTimeElement.innerHTML = `${tokyoTime.format("h:mm:ss [<small>]A[</small>]")}`;
+}
+
+}
+
+function displayCity(event) {
     let cityTimeZone = event.target.value;
+    let cityName = event.target.options[event.target.selectedIndex].text;
+    clearInterval(interval);
+
+    updateCityInterval(cityTimeZone, cityName);
+    interval = setInterval(() => {
+        updateCityInterval(cityTimeZone, cityName);
+    }, 1000);
+}
+
+
+ function updateCityInterval(cityTimeZone, cityName) {   
     if (cityTimeZone === "current") {
         cityTimeZone = moment.tz.guess();
+        cityName = cityTimeZone.replace("_", " ").split("/")[1];
     }
-    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+    
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector("#cities");
+
+    if (cityTimeZone.length > 0) {
     citiesElement.innerHTML = `
     <div class="city">
           <div>
@@ -43,10 +68,17 @@ function updateCity(event) {
         </div>
         <a href="/">All cities</a>
         `;
+        } else {
+            location.reload();
+        }
 
 }
+
+let interval;
+displayTime();
+interval = setInterval(displayTime, 1000);
 
 
 let citySelect = document.querySelector("#city");
 
-citySelect.addEventListener("change", updateCity);
+citySelect.addEventListener("change", displayCity);
